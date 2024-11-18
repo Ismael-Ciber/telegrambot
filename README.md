8ef697b3ecde9ac24da89edd28290e415470a4ab
ismael <wanchito0404@gmail.com>
2024-11-18 16:54:15 +0100

    Primer commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Telegram Bot
+
+## Configuración
+
+1. Busca el usuario `BotFather` en telegram
+2. Crea un nuevo bot
+3. Añade tu API_KEY a .env_ejemplo y renombrelo a .env
+4. Crea un entorno virtual con `python3 -m venv env`
+5. Activa el entorno virtual con `source env/bin/activate`
+6. Baja las dependencias con `pip install -r requierement`
+7. Lanza el bot con `python main.py`
---
+++ main.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import subprocess
+
+async def saludo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Hola!")
+
+async def scan(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    domain = context.args[0]
+    print(domain)
+    resultados = subprocess.run(["./scan.sh", domain], capture_output=True, text=True, check=True )
+    await update.message.reply_text(resultados.stdout)
+
+def main():
+    app = ApplicationBuilder().token(API_KEY).build()
+    app.add_handler(CommandHandler("saludo", saludo))
+    app.add_handler(CommandHandler("scan", scan))
+    print ("Bot iniciado")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
